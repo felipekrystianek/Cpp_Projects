41ef9c8359b7c49d1ef7965372e65b587e480a01
Krystian Filipek <filipek.krystian723@gmail.com>
2023-10-05 21:32:34 +0200

    From my first lecture :>

---
+++ 1_app.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Krystian Filipek on 05/10/2023.
+//
+
+#include <iostream>
+
+double maks = -1;
+double liczba;
+
+int main() {
+    for (int i = 0; i < 4; i++) {
+        std::cin >> liczba;
+
+        if (liczba < 0) {
+            break;
+        }
+
+        if (liczba > maks) {
+            maks = liczba;
+        }
+    }
+
+    std::cout << "Wartość maksymalna: " << maks << std::endl;
+
+    return 0;
+}
